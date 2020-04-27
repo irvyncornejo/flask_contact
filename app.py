abd6a2ec64f477c268d90e5313f1d0acf2c65dd3
Irvyn <irvyncornejo@gmail.com>
2020-04-27 03:11:04 -0500

    Las operaciones de CRUD ya están en completas, se añadió un framework para los estilos y se integró un layout

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+#MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '2012350818CFFDA'
@@ -9,9 +11,15 @@ app.config['MYSQL_DB'] = 'contacts'
 app.config['MYSQL_PORT'] = 3307
 mysql = MySQL(app)
 
+#Session
+app.secret_key = 'millavesecreta'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute(' SELECT * FROM contacts_table')
+    data = cur.fetchall()
+    return render_template('index.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def addContact():
@@ -25,18 +33,39 @@ def addContact():
                     'VALUES (%s, %s, %s )', (fullname, phone, email))
 
         mysql.connection.commit()
-
+        flash('Contact added successfully')
         print('{} | {} | {}'.format(fullname, phone, email))
-        return 'Datos recibidos'
-@app.route('/edit')
-def editContact():
-    return 'Edit contact'
+        return redirect(url_for('index'))
 
-@app.route('/delete')
-def deleteContact():
-    return 'Delete contact'
+@app.route('/edit/<id>')
+def get_contact(id): 
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts_table WHERE user_id={}'.format(id))
+    data = cur.fetchone()
+    print(data)
+    return render_template('edit-contact.html', contact = data)
 
+@app.route('/delete/<string:id>')
+def deleteContact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM  contacts_table WHERE user_id= {}'.format(id))
+    mysql.connection.commit()
+    flash('Contact deleted')
+    return redirect(url_for('index'))
 
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+
+    cur = mysql.connection.cursor()
+    cur.execute('UPDATE contacts_table SET full_name=%s, phone =%s, email=%s WHERE user_id=%s' , (fullname, phone, email, id))
+    mysql.connection.commit()
+    
+    flash('Contact updated')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(port= 5000, debug= True)
